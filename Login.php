6eb65dea9c464766bee43455fa5e19b0803ee0d7
Felipe Almeida <felpson654@gmail.com>
2026-09-22 14:20:25 -0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,15 +1,19 @@
 <?php
+session_start();
 $senha=$_POST['senha'];
 $email=$_POST['email'];
 $conexão=mysqli_connect('localhost','root','','teste_PHP');
 $busca=mysqli_query($conexão,"SELECT*FROM login WHERE email='$email' and senha='$senha'");
-
 $contagem=mysqli_num_rows($busca);
 
 if($contagem==1){
-    echo "Tu estás logado de forma perpetua";
+    $_SESSION['Log']=1;
+    header("Location: Logado.php");
+
 }
 else{
+     $_SESSION['Log']='0';
     echo "Você não está logado >:(";
+    header("Location: Logado.php");
 }
 ?>
